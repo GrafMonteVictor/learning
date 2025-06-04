f1b8f9c3bffe38934167501d816eb76f0599e56d
grafMonteVictor <victorpindrus@gmail.com>
2025-06-04 05:24:00 +0300

    init - выход из нити: return

--- multithreading/multithreading/src/main/java/org/example/creatingThreads/App1.java
+++ multithreading/multithreading/src/main/java/org/example/creatingThreads/App1.java
@@ -10,7 +10,7 @@ public class App1 implements Runnable
     public void run() {
         for (int i = 0; i < 100; i++) {
             if (i == 5) {
-                return;
+                return; //exit
             }
             try {
                 Thread.sleep(1000);
