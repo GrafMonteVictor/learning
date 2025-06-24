db63d1a6792d8644aeef517262313c62aae96f9d
grafMonteVictor <victorpindrus@gmail.com>
2025-06-25 00:11:51 +0300

    refactoring

--- collections/collections/src/main/java/org/example/lambda/functionalInterface/Main.java
+++ collections/collections/src/main/java/org/example/lambda/functionalInterface/Main.java
@@ -3,9 +3,7 @@ package org.example.lambda.functionalInterface;
 public class Main {
     public static void main(String[] args) {
         StringProcessor sp = new StringProcessor();
-        sp.setCharacterReplacer((input, oldChar, newChar) -> {
-            return input.replace(oldChar, newChar);
-        });
+        sp.setCharacterReplacer((input, oldChar, newChar) -> input.replace(oldChar, newChar));
 
         sp.setLengthChecker(input -> {
             if (input.length() > 10) {
@@ -14,8 +12,12 @@ public class Main {
                 System.out.println("String length is fine.");
             }
         });
+
         sp.setUpperCaseConverter(input -> {
             return input.toUpperCase();
         });
+
+        String result = sp.process("просто так");
+        System.out.println(result);
     }
 }
